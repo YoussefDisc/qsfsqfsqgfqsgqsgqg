d6c1c9cafe2d3c283e7d0529b6b0999e9819af07
YoussefDisc <54398960+YoussefDisc@users.noreply.github.com>
2019-08-22 12:34:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,40 @@
-const Discord = require('discord.js');
-const client = new Discord.Client(); 
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+
+const Guild = "211543198651121664";
+const voiceChannel = "613754309036998656";
 
 client.on("ready", () => {
-  console.log(client.user.tag + " Ready!");
-  const channel = client.channels.get("613754309036998656");
-  channel.join().then(connection => {
-    console.log("Successfully connected.");
-  }).catch(e => {
-    console.error(e);
+  console.log(client.user.tag + " Ready! (" + client.guilds.get(Guild).name + ', ' + client.guilds.get(Guild).channels.get(voiceChannel).name + ')');
+  client.channels.get(voiceChannel).join().then(connection => {
+    console.log(client.user.tag + " Successfully connection.");
+    setInterval(() => {
+      if(!client.guilds.get(Guild).members.get(client.user.id).voiceChannel || client.guilds.get(Guild).members.get(client.user.id).voiceChannel && client.guilds.get(Guild).members.get(client.user.id).voiceChannelID != voiceChannel) {
+        client.channels.get(voiceChannel).join().then(connection => {
+          console.log(client.user.tag + " Successfully connection.");
+        });
+      }
+    }, 5000);
   });
 });
 
 client.login(process.env.TOKEN);
 
-
-client.on("ready", () => {
-  console.log(client.user.tag + " Ready!");
-  const channel = client.channels.get("613754309036998656");
-  channel.join().then(connection => {
-    console.log("Successfully connected.");
-  }).catch(e => {
-    console.error(e);
+lient.on("ready", () => {
+  console.log(client.user.tag + " Ready! (" + client.guilds.get(Guild).name + ', ' + client.guilds.get(Guild).channels.get(voiceChannel).name + ')');
+  client.channels.get(voiceChannel).join().then(connection => {
+    console.log(client.user.tag + " Successfully connection.");
+    setInterval(() => {
+      if(!client.guilds.get(Guild).members.get(client.user.id).voiceChannel || client.guilds.get(Guild).members.get(client.user.id).voiceChannel && client.guilds.get(Guild).members.get(client.user.id).voiceChannelID != voiceChannel) {
+        client.channels.get(voiceChannel).join().then(connection => {
+          console.log(client.user.tag + " Successfully connection.");
+        });
+      }
+    }, 5000);
   });
 });
 
 client.login(process.env.TOKEN2);
+
